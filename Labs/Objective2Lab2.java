709bbe6598af6d20124edad0463138d3dcfae7a7
Kenneth Ratliff <kenneth03895@gmail.com>
2023-09-11 19:16:54 -0500

    Objective2Lab2.java

---
+++ Labs/Objective2Lab2.java
@@ -0,0 +1,9 @@
+public class Objective2Lab2 {
+  public static void main(String[] args) {
+    String name = "Kenneth Ratliff"; 
+    int age = 28;
+    String state = "Louisiana";
+      System.out.println("name: " + name + " Age: " + age + " state: " + state);
+    
+  }
+}
