868e153cbfff9e72822ecb4d0a2f168245372fc4
Kenneth Ratliff <kenneth03895@gmail.com>
2023-09-11 19:41:50 -0500

    Objective2Lab3.java

---
+++ Labs/Objective2Lab3.java
@@ -0,0 +1,11 @@
+public class Objective2Lab3 {
+  public static void main(String[] args) {
+     int birthYear = 1995;
+     String birthMonth = "september";
+     boolean weekendorNot = false;
+         System.out.println(birthYear);
+         System.out.println(birthMonth);
+         System.out.println(weekendorNot);
+
+  }
+}
